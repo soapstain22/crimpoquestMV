bb8020d0da674625ea7fe6a354177e52e6614cd3
max <soapstain22@gmail.com>
2022-10-09 13:05:20 -0400

    great

--- js/plugins/ccedits.js
+++ js/plugins/ccedits.js
@@ -1,20 +1,6 @@
-function givemetheitem(itemid){
-	var parameters = PluginManager.parameters('ItemBook');
-	var item = $dataItems.at(itemid)
-	var commanded = Game_Interpreter.prototype.pluginCommand;
-
-	Game_Interpreter.pluginCommand = function(a, b){
-		_Game_Interpreter_pluginCommand.call(this, item, args);
-		if (item = 'money'){
-			Game_Party.prototype.gainGold(args)
-			ItemGetWindow("Money", args, 40)
-		}
-	}
-	
+function Scene_GetItem(){
+	this.initialize.apply(this,arguments);
 }
-function ItemGetWindow(toptext, bottomtext, id){
-	Window_Base.prototype.initialize(screen.availWidth/2,screen.availHeight/2,200,80)
-	Window_Base.prototype.create.call(this)
-	
-	
-}
+Scene_GetItem.prototype = Object.create(Scene_MenuBase.prototype);
+Scene_GetItem.prototype.constructor = Scene_GetItem;
+Scene_GetItem
