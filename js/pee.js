75c7886bea523acb63a728317d2a6ca7c15b9683
max <soapstain22@gmail.com>
2022-11-25 14:43:20 -0500

    ugh

--- js/pee.js
+++ js/pee.js
@@ -1,14 +1,21 @@
-function s() {
-	Game_Interpreter.prototype.pluginCommand = function(command, args) {
-		if (args[0] = 'money'){
-			Game_Party.prototype.gainGold(args)
-			SceneManager.push(ItemGetWindow)
-			ItemGetWindow(money,20)
-		}
-	}
-	Scene_Base.prototype.ItemGetWindow(item, ammount){
-		item = $dataItems.findIndex(args[0])
-		
-	};
-	}
-};
+function Scene_Obtain(){
+	this.initialize.apply(this, arguments)
+}
+Scene_Obtain.prototype = Object.create(Scene_MenuBase.prototype)
+Scene_Obtain.prototype.constructor = Scene_Obtain;
+
+Scene_Obtain.prototype.initialize() = function() {
+	Scene_MenuBase.prototype.initialize.call(this);
+}
+Scene_Obtain.prototype.create = function(){
+	Scene_Obtain.prototype.create.call
+	this.customWindo
+}
+function Window_Test(){
+	this.initialize.apply(this, arguments);
+}
+Window_Test.prototype = Object.create(Window_Base.prototype);
+Window_Test.prototype.constructor = Window_Test;
+Window_Test.prototype.initialize = function(x,y,width,height) {
+	Window_Base.prototype.initialize.call(this,x,y,width,height);
+}
